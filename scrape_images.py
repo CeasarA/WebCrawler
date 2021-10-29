973bac3d68d423cb781302e6de0d2fbd4e17f5af
CeasarA <ceasarkwadwo@gmail.com>
2021-10-29 11:14:32 +0000

    images from csv

--- scrape_images.py
+++ scrape_images.py
@@ -1,8 +1,7 @@
 import requests
-from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
-
+import pandas as pd
 
 url = 'https://nsano.com/'
 
@@ -21,7 +20,6 @@ raw_links = soup.find_all("img")
 # loop through raw links and process them
 links = []
 for i in raw_links:
-    print('i', i)
     link = i['src']
     if link.startswith("http"):
         links.append(link)
@@ -29,7 +27,15 @@ for i in raw_links:
         new_link = "https://" + domain + '/' + link
         links.append(new_link)
 
-# Write and Dwonload Images to a File
-for image in links:
-    
 
+print("Processed And Cleaned Links: ", links)
+
+# Write and Dwonload Images to a File
+for x in links:
+    image = requests.get(x).content
+    # print(image)
+    break
+
+# Export data to a csv file
+df = pd.DataFrame({"Links": links})
+df.to_csv('images_from_nsano.csv', index=False, encoding='utf-8')
