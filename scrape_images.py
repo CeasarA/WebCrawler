57058c64e76019b0cb1896688ebcac19f11ff4f0
CeasarA <ceasarkwadwo@gmail.com>
2021-10-29 12:01:25 +0000

    alkskd

--- scrape_images.py
+++ scrape_images.py
@@ -1,7 +1,14 @@
+from os import name
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 import pandas as pd
+from PIL import Image
+#io manages file-related in/out operations
+import io
+import pathlib
+import hashlib
+
 
 url = 'https://nsano.com/'
 
@@ -30,12 +37,22 @@ for i in raw_links:
 
 print("Processed And Cleaned Links: ", links)
 
-# Write and Dwonload Images to a File
-for x in links:
-    image = requests.get(x).content
-    # print(image)
-    break
-
 # Export data to a csv file
 df = pd.DataFrame({"Links": links})
 df.to_csv('images_from_nsano.csv', index=False, encoding='utf-8')
+
+
+# Write and Dwonload Images to a File
+for x in links:
+    image = requests.get(x).content
+    #creates a byte object out of image_content and point the variable image_file to it
+    image_file = io.BytesIO(image)
+    image = Image.open(image_file).convert('RGB')
+    # file_path = pathlib.Path('/images', hashlib.sha1(image).hexdigest()[:10] + '.png')
+    file_path = pathlib.Path('images')
+    image.save(file_path, "PNG", quality=80)
+    print(image_file)
+
+
+if __name__ == "__main__":
+    print('main')
