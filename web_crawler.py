978061b0942ca781ff963efca862ae8979e2e9fa
CeasarA <ceasarkwadwo@gmail.com>
2021-10-29 09:18:48 +0000

    initial commit

--- web_crawler.py
+++ web_crawler.py
@@ -1,4 +1,16 @@
 from urllib.request import urlopen
 
-url = "htt"
+url = "http://olympus.realpython.org/profiles/aphrodite"
 
+# Open the url
+page = urlopen(url)
+
+# Return a sequence of bytes
+html_bytes = page.read()
+
+# Decode bytes to a string
+html = html_bytes.decode("utf-8")
+
+# Find and Extract Title
+title_index = html.find("<title>")
+print(title_index)
