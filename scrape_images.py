ddc62eb15bf7d567cd0ba854bd166c91c23e8ab2
CeasarA <ceasarkwadwo@gmail.com>
2021-10-29 10:58:02 +0000

    scrape images from nsano

--- scrape_images.py
+++ scrape_images.py
@@ -6,25 +6,30 @@ from urllib.parse import urlparse
 
 url = 'https://nsano.com/'
 
+# Extract domain
 domain = urlparse(url=url).netloc
-print('doamin', domain)
 
-requests = requests.get(url)
-print('reques', requests)
+# Make a request to url, It will return a response
+responses = requests.get(url)
 
-soup = BeautifulSoup(requests.text, "html.parser")
-print('soup', soup)
+# Parse the response through BS4's html parser
+soup = BeautifulSoup(responses.text, "html.parser")
 
+# Extract all tags with 'img' in it
 raw_links = soup.find_all("img")
-print('raw_links', raw_links)
 
+# loop through raw links and process them
 links = []
-for i in links:
+for i in raw_links:
+    print('i', i)
     link = i['src']
     if link.startswith("http"):
         links.append(link)
     else: 
-        new_link = "https://" + domain + link
+        new_link = "https://" + domain + '/' + link
         links.append(new_link)
 
+# Write and Dwonload Images to a File
+for image in links:
+    
 
