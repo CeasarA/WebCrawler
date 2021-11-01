7ce49c6a3b86a83003292dc5a39e1296d39fe136
CeasarA <ceasarkwadwo@gmail.com>
2021-11-01 14:15:45 +0000

    writing unit test

--- test_logo_crawler.py
+++ test_logo_crawler.py
@@ -7,29 +7,30 @@ class TestLogoCrawler(unittest.TestCase):
     """"
     Set up test fixed variables
     """
-    def stepUp(self):
-        self.url = 'https://nsano.com'
+    def setUp(self):
+        self.url = "https://nsano.com"
+        self.get_url_response = get_url(self.url)
 
     def test_url_starts_with_https(self):
         """Whether links starts with https"""
-        url = "https://nsano.com"
-        self.assertAlmostEqual(url.startswith("https"), True)
+        self.assertAlmostEqual(self.url.startswith("https"), True)
 
     def test_url_exists(self):
-        "test whether the url exists"
-        url = "https://nsano.com"
-        get_url(url)
-        url_response = requests.get(url=url)
+        "whether the url exists"
+        # get_url(self.url)
+        url_response = requests.get(url=self.url)
         assert url_response.status_code < 400
 
-    def test_get_url_return_a_list(self):
-        url = "https://nsano.com"
+    def test_get_url_returns_an_image(self):
+        "whether the get_url function returns an image"
         image_formats = ("image/png", "image/jpeg", "image/jpg")
-        return_value = get_url(url)
-        r = requests.head(return_value)
+        r = requests.head(self.get_url_response)
         format = r.headers["content-type"] in image_formats
-        print(format)
-        self.assertEqual(url, return_value)
+        self.assertEqual(format, True)
+
+
+    def tearDown(self) -> None:
+        return super().tearDown()
 
 if __name__ == '__main__':
     unittest.main()
