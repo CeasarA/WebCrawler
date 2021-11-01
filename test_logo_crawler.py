57fd0db1eab00af751a8791a5a354e440c461cfc
CeasarA <ceasarkwadwo@gmail.com>
2021-11-01 13:49:24 +0000

    writing unit test

--- test_logo_crawler.py
+++ test_logo_crawler.py
@@ -16,11 +16,20 @@ class TestLogoCrawler(unittest.TestCase):
         self.assertAlmostEqual(url.startswith("https"), True)
 
     def test_url_exists(self):
+        "test whether the url exists"
         url = "https://nsano.com"
         get_url(url)
         url_response = requests.get(url=url)
         assert url_response.status_code < 400
 
+    def test_get_url_return_a_list(self):
+        url = "https://nsano.com"
+        image_formats = ("image/png", "image/jpeg", "image/jpg")
+        return_value = get_url(url)
+        r = requests.head(return_value)
+        format = r.headers["content-type"] in image_formats
+        print(format)
+        self.assertEqual(url, return_value)
 
 if __name__ == '__main__':
     unittest.main()
